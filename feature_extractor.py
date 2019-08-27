7510c573bcca8c3c2d913cf16d28f206b0b60bf1
qiutingli9 <50158267+qiutingli9@users.noreply.github.com>
2019-08-27 19:29:16 +1000

    Commands Added

--- feature_extractor.py
+++ feature_extractor.py
@@ -10,14 +10,17 @@ class FeatureExtractor:
         self.tweet = tweet
         self.tokenized_tweet = word_tokenize(tweet)
 
+
+    # Determine if the tweet is a retweet. Return 1 if it's a retweet, return 0 otherwise.
     def determine_retweet(self):
-        # return 1 if it's a retweet, return 0 otherwise.
         if self.tokenized_tweet[0] == 'RT':
             return 1
         else:
             return 0
 
+    # Return number of words in tweet.
     def get_num_of_words(self):
+
         return len(self.tokenized_tweet)
 
 
@@ -36,6 +39,7 @@ if __name__ == '__main__':
         num_of_words = extractor.get_num_of_words()
         featured_dict = {'id': tweets.iloc[i]['id'], 'retweet_or_not': retweet_or_not, 'num_of_words': num_of_words}
         featured_df = featured_df.append(featured_dict, ignore_index = True)
+
     print(featured_df)
 
 
