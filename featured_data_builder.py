31866aa2ed5aac7b0ed3136c494cc3179f834dbc
qiutingli9 <50158267+qiutingli9@users.noreply.github.com>
2019-08-27 23:45:14 +1000

    Added writing featured df to file functionality

--- featured_data_builder.py
+++ featured_data_builder.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize
 
 pd.set_option("display.max_columns", 500)
 
+
 class FeatureExtractor:
 
     def __init__(self, tweet):
@@ -33,61 +34,76 @@ class FeatureExtractor:
         urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', self.tweet)
         return urls
 
-# Initialize featured data frame by using given column names.
-def initialize_featured_data_frame():
-    return pd.DataFrame(columns = featured_df_columns)
 
 
-# Construct featured data frame iterating the original data.
-def construct_featured_data_frame(featured_df):
+class FeaturedDataBuilder:
 
-    for i in range(len(original_training_data))[1:10]:
-        tweet = original_training_data.iloc[i]['tweet']
-        extractor = FeatureExtractor(tweet)
+    def __init__(self, original_data):
+        self.featured_df_columns = ['id',
+                                    'retweet_or_not', 'num_of_words', 'num_of_hashtags', 'hashtag_contents',
+                                    'num_of_mentions', 'num_of_urls', 'url_contents']
+        self.original_data = original_data
 
-        retweet_or_not = extractor.determine_retweet()
-        num_of_words = extractor.get_num_of_words()
-        hashtag_contents = extractor.get_hashtag_contents()
-        num_of_hashtags = len(hashtag_contents)
-        num_of_mentions = extractor.get_num_of_mentions()
-        url_contents = extractor.get_urls()
-        num_of_urls = len(url_contents)
 
-        featured_dict = {
-            featured_df_columns[0]: original_training_data.iloc[i]['id'],
-            featured_df_columns[1]: retweet_or_not,
-            featured_df_columns[2]: num_of_words,
-            featured_df_columns[3]: num_of_hashtags,
-            featured_df_columns[4]: hashtag_contents,
-            featured_df_columns[5]: num_of_mentions,
-            featured_df_columns[6]: num_of_urls,
-            featured_df_columns[7]: url_contents
-        }
-        featured_df = featured_df.append(featured_dict, ignore_index=True)
+    # Initialize featured data frame by using given column names.
+    def initialize_featured_data_frame(self):
+        return pd.DataFrame(columns = self.featured_df_columns)
 
-    return featured_df
 
-def write_featured_df_to_csv(featured_df, file):
-    if os.path.exists(file):
-        featured_df.to_csv(file)
-    else:
-        try:
-            os.makedirs(file)
-        except Exception as e:
-            print(e)
-        featured_df.to_csv(file, index=False, encoding="utf-8")
+    # Construct featured data frame iterating the original data.
+    def construct_featured_data_frame(self):
+        featured_df = self.initialize_featured_data_frame()
+        for i in range(len(self.original_data))[1:10]:
+            tweet = self.original_data.iloc[i]['tweet']
+            extractor = FeatureExtractor(tweet)
+
+            retweet_or_not = extractor.determine_retweet()
+            num_of_words = extractor.get_num_of_words()
+            hashtag_contents = extractor.get_hashtag_contents()
+            num_of_hashtags = len(hashtag_contents)
+            num_of_mentions = extractor.get_num_of_mentions()
+            url_contents = extractor.get_urls()
+            num_of_urls = len(url_contents)
+
+            featured_dict = {
+                self.featured_df_columns[0]: original_training_data.iloc[i]['id'],
+                self.featured_df_columns[1]: retweet_or_not,
+                self.featured_df_columns[2]: num_of_words,
+                self.featured_df_columns[3]: num_of_hashtags,
+                self.featured_df_columns[4]: hashtag_contents,
+                self.featured_df_columns[5]: num_of_mentions,
+                self.featured_df_columns[6]: num_of_urls,
+                self.featured_df_columns[7]: url_contents
+            }
+            featured_df = featured_df.append(featured_dict, ignore_index=True)
+
+        self.featured_df = featured_df
+        return self.featured_df
+
+
+    def write_featured_df_to_csv(self, file):
+        if os.path.exists(file):
+            self.featured_df.to_csv(file)
+        else:
+            try:
+                os.makedirs(file)
+            except Exception as e:
+                print(e)
+            self.featured_df.to_csv(file, index=False, encoding="utf-8")
+
 
 
 if __name__ == '__main__':
+
+    # Read original training dataset
     training_data_path = "%s/data/train_tweets.txt" % os.path.abspath('.')
     original_training_data = pd.read_csv(training_data_path, sep ="\t", names = ['id', 'tweet'])
-    featured_df_columns = ['id', 'retweet_or_not', 'num_of_words', 'num_of_hashtags', 'hashtag_contents',
-                           'num_of_mentions', 'num_of_urls', 'url_contents']
-    featured_df = initialize_featured_data_frame()
-    featured_df = construct_featured_data_frame(featured_df)
 
+    # Transform original training dataset to featured dataset
+    featured_data_builder = FeaturedDataBuilder(original_training_data)
+    featured_df = featured_data_builder.construct_featured_data_frame()
     featured_training_df_path = "%s/data/featured_training_df.csv" % os.path.abspath('.')
-    write_featured_df_to_csv(featured_df, featured_training_df_path)
+    featured_data_builder.write_featured_df_to_csv(featured_training_df_path)
 
     # print(featured_df)
     # print(pd.read_csv(featured_training_df_path, sep=',').head())
